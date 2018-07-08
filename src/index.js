20a652df49ccc4b51bcba14b95551db4d9a5cbee
Collier Hansen <cole.hansen@gmail.com>
2018-07-08 15:08:43 -0700

    Working app with search

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
+import _ from 'lodash';
 import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
-import VideoList from './components/video_list';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
 
 const API_KEY = 'AIzaSyAa5cxdsWSxpt5MDciiMoz0TYAS3kBvQbU';
-
-
 
 // Create a new component. This component should produce some HTML
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             // because the key and value are the same (videos) we can change it from videos: videos to just videos
-            this.setState({ videos });
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
             </div>
             )
     }

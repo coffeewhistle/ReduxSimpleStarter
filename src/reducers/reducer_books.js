718da54fe3b1ea7469063c92dcb224389c734d44
System Administrator <root@Colliers-MacBook-Pro.local>
2018-07-11 21:09:53 -0700

    Setup redux reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Hitchhiker\'s Guide to the Galaxy'},
+        { title: 'Another book'},
+        { title: 'I Can Do it'},
+        { title: 'Last Book'}
+    ]
+}
